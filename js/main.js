d9eefb82da40658f598c9d6fe4b00a04e6c8f56b
webilm <web9ilm@gmail.com>
2023-11-26 17:36:12 +0500

    GitHub Ilm

---
+++ js/main.js
@@ -0,0 +1,86 @@
+"use strict";
+
+//   6 dars Interpolatsiya 
+
+// const channel = prompt("What's your favourite you tuber");
+// console.log(`https://www.youtube.com/${channel}`)
+
+// let person = {
+//     name1: "Mahmud",
+//     age: 26;
+//     weAre: "Toshkent";
+//     book: "Ilon Mask";
+// }
+
+// console.log(person.age)
+
+// const ism = "Mahmud";
+// const yosh = 26;
+// const yashashJoyi = "Toshkent";
+
+// console.log(`Ismingiz ${ism} yoshingiz ${yosh} yashash joyingiz ${yashashJoyi}`)
+
+
+
+                   // 7 dars  Operatorlar
+
+// console.log(6 + "9");
+
+// incr  decr haqida
+
+// let incr = 10;
+// let decr = 10;
+
+// incr++
+// decr--
+
+// console.log(incr);
+// console.log(decr);
+
+//   ++incr  --decr
+// console.log(++incr);
+// console.log(--decr);
+
+//    incr++ decr--
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr++);
+// console.log(decr--);
+
+
+
+// let b = 10;
+
+// console.log(b++); // 10 (avratta b qiymati 11 bo'ladi)
+// console.log(b); // 11
+
+// let c = 15;
+
+// console.log(++c); // 16 (avratta c qiymati 16 bo'ladi)
+// console.log(c); // 16
+
+// let qiymat = 10
+// let qiymat2 = 15
+
+// // qiymat++;
+// // qiymat2--;
+
+// console.log(++qiymat)
+// console.log(--qiymat2)
+
+// qoldiq deyiladi 5 % 2
+console.log(5 % 2)
+
+console.log(6 % 2)
+
+
+// && = va || = yoki
+const isAge = true;
+const isClose = true;
+const isChecket = false;
+
+console.log(isAge && isClose && !isChecket);
